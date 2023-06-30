ede91c7614679e39fbb2608107a29f0fea0ddea7
Shaw Ho <tossmilestone@gmail.com>
2023-06-30 11:15:10 +0800

    Fix language argument used error in prompt
    Signed-off-by: Shaw Ho <tossmilestone@gmail.com>

--- pkg/ai/prompts.go
+++ pkg/ai/prompts.go
@@ -1,7 +1,7 @@
 package ai
 
 const (
-	default_prompt = `Simplify the following Kubernetes error message delimited by triple dashes written in --- %s --- language; --- %s ---.
+	default_prompt = `Simplify the following Kubernetes error message delimited by triple dashes written in %s language; --- %s ---.
 	Provide the most possible solution in a step by step style in no more than 280 characters. Write the output in the following format:
 	Error: {Explain error here}
 	Solution: {Step by step solution here}
